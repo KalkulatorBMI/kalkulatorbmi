c732403e538272ed9f5b6a95980ae1aba94d5bca
Adrian Jankowski <jankowski.adrian13@gmail.com>
2016-05-17 00:33:46 +0200

    Rozwój aplikacji

---
+++ app/src/main/java/com/example/adrian/appfit/NoteClass.java
@@ -0,0 +1,7 @@
+package com.example.adrian.appfit;
+
+/**
+ * Created by Adrian on 16.05.2016.
+ */
+public class NoteClass {
+}
---
+++ app/src/main/java/com/example/adrian/appfit/PlanDzialania.java
@@ -0,0 +1,13 @@
+package com.example.adrian.appfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlanDzialania extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_dzialania);
+    }
+}
